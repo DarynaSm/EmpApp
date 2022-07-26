23cf25ea07249c55ecd058b6947c869085502438
Daryna Smok <sdm281203@gmail.com>
2022-07-26 15:03:43 +0300

    added RegisterViewModel

---
+++ Test/Models/RegisterViewModel.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace Test.Models
+{
+    public class RegisterViewModel
+    {
+        [Required]
+        [EmailAddress]
+        public string Email { get; set; }
+
+        [Required]
+        [DataType(DataType.Password)]
+        public string Password { get; set; }
+
+        [DataType(DataType.Password)]
+        [Display(Name="ConfirmPassword")]
+        [Compare("Password",ErrorMessage ="please write the same password")]
+        public string ConfirmPassword{ get; set; }
+
+        [Required]
+        public string Name { get; set; }
+
+        [Required]
+        public string Surname { get; set; }
+
+        [Required]
+        [DataType(DataType.PhoneNumber)]
+        public string PhoneNumber { get; set; }
+
+        public string City { get; set; }
+        public string Age { get; set; }
+
+
+        public RegisterViewModel()
+        {
+        }
+    }
+}
